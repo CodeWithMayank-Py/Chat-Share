c243e3bfc3919eaf86fb8dbf8b3a5df28eed271b
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-03-06 16:07:47 +0530

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -17,3 +17,7 @@ app.secret_key = generate_secret_key()
 
 # Import routes from the auth module
 from . import routes
+
+# If needed, include the run logic directly for development:
+if __name__ == "__main__":
+    app.run(debug=True)         # Run the Flask app in debug mode
