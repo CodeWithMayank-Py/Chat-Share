9b130ec5d80b5ebb248b0478f7efe1ed9960968a
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-03-07 11:50:01 +0530

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,10 @@ from flask import Flask
 import os
 from .utils import generate_secret_key
 
-# Get the absolute path of the current directory
-current_dir = os.path.dirname(__file__)
-
-# Define the paths relative to the current directory
-template_folder = os.path.join(current_dir, 'templates')
-static_folder = os.path.join(current_dir, 'static')
-
 # Create the Flask application instance
-app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+app = Flask(__name__, 
+                 template_folder='C:\\Users\\paliw\\Documents\\YT Playlist\\chat-share\\app\\templates', 
+                 static_folder='C:\\Users\\paliw\\Documents\\YT Playlist\\chat-share\\static')
 
 # Set the secret key for session management
 app.secret_key = generate_secret_key()
