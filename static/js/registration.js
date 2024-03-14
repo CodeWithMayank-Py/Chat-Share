ee5df67c898842539b91034fac5b35fa39ba98fe
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-03-14 23:55:45 +0530

    Bugs Fixes

--- static/js/registration.js
+++ static/js/registration.js
@@ -1,37 +1,37 @@
 // Function for toggling
 const toggleForm = () => {
-  const container = document.querySelector('.container');
-  container.classList.toggle('active');
+    const container = document.querySelector('.container');
+    container.classList.toggle('active');
 };
 
 
 // Function to validate password
 const validatePassword = () => {
-  const password = document.getElementById('password').value;
-  const confirm_password = document.getElementById('confirm_password').value;
-  const passwordError = document.getElementById('passwordError');
+    const password = document.getElementById('password').value;
+    const confirm_password = document.getElementById('confirm_password').value;
+    const passwordError = document.getElementById('passwordError');
 
-  if (password !== confirm_password) {
-    passwordError.style.display = 'block'; // Show error message
-    return false; // Prevent form submission
-  } else {
-    passwordError.style.display = 'none'; // Hide error message
-    return true; // Proceed with form submission
-  }
+    if (password !== confirm_password) {
+      passwordError.style.display = 'block'; // Show error message
+      return false; // Prevent form submission
+    } else {
+      passwordError.style.display = 'none'; // Hide error message
+      return true; // Proceed with form submission
+    }
 };
 
 
 // Function to check password instantly when user types in confirm password field
 const checkPasswordInstantly = () => {
-  const password = document.getElementById('password').value;
-  const confirm_password = document.getElementById('confirm_password').value;
-  const passwordError = document.getElementById('passwordError');
+    const password = document.getElementById('password').value;
+    const confirm_password = document.getElementById('confirm_password').value;
+    const passwordError = document.getElementById('passwordError');
 
-  if (password !== confirm_password) {
-    passwordError.style.display = 'block'; // Show error message
-  } else {
-    passwordError.style.display = 'none'; // Hide error message
-  }
+    if (password !== confirm_password) {
+      passwordError.style.display = 'block'; // Show error message
+    } else {
+      passwordError.style.display = 'none'; // Hide error message
+    }
 };
 
 
@@ -41,14 +41,14 @@ document.getElementById('confirm_password').addEventListener('input', checkPassw
 
 // Function to toggling the password visibility
 const togglePasswordVisibility = (inputId) => {
-  const passwordInput = document.getElementById(inputId);
-  const eyeIcon = document.querySelector(`span.toggle-password[onclick="togglePasswordVisibility('${inputId}')"]`);
+    const passwordInput = document.getElementById(inputId);
+    const eyeIcon = document.querySelector(`span.toggle-password[onclick="togglePasswordVisibility('${inputId}')"]`);
 
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    eyeIcon.textContent = "👁️";
-  } else {
-    passwordInput.type = "password";
-    eyeIcon.textContent = "🔒";
-  }
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      eyeIcon.textContent = "👁️";
+    } else {
+      passwordInput.type = "password";
+      eyeIcon.textContent = "🔒";
+    }
 };
